e927471153fcc7a7ba5346ec6b67734987db4ad8
Grigtron <jgregas@gmail.com>
2024-10-06 21:22:52 -0400

    Added shot limiter

--- player.py
+++ player.py
@@ -11,6 +11,7 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         CircleShape.radius = PLAYER_RADIUS
         self.rotation = 0
+        self.shoot_timer = 0
         if Player.containers:
             for container in Player.containers:
                 container.add(self)
@@ -42,13 +43,16 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_SPACE]:
             self.shoot()
+        
+        self.shoot_timer -= dt
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
+        if self.shoot_timer > 0:
+            return
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
-        
-        
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN      
